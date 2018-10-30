980fbc31933be0c1828d4863bcf1efabac7c7059
Elina Razina <razina_elina@mail.ru>
2018-10-30 18:47:47 +0300

    вывод списка из массива

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'World!';
+  users = [
+    {name: 'Вася'},
+    {name: 'Ира'},
+    {name: 'Настя'},
+    {name: 'Алина'},
+    {name: 'Барсик'},
+    {name: 'Ваня'}
+  ];
 }
